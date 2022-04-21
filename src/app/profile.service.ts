6d27b7d8c43b708b604d4b9f62928e898f148c5e
haimy254 <haimanaare@gmail.com>
2022-04-21 19:05:01 +0300

    api connected

--- src/app/profile.service.ts
+++ src/app/profile.service.ts
@@ -1,19 +1,17 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { lastValueFrom } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ProfileService {
-  [x: string]: any;
-  BaseUrl = 'https://api.github.com/';
-  user: any = 'haimy254';
+  BaseUrl = 'https://api.github.com';
 
-  constructor(private http: HttpClient) {
-    async () => {
-      let response = await http.get(`${this.BaseUrl}/users/${this.user}`);
-      console.log(response)
-    };
+  constructor(private http: HttpClient) {}
+
+  async getUser(userName: string) {
+    let response = this.http.get(`${this.BaseUrl}/users/${userName}`);
+    return await lastValueFrom(response).then((result) => result);
   }
 }
--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,19 +1,24 @@
-
 import { Component, OnInit } from '@angular/core';
 import { Profile } from '../profile/profile';
+import { ProfileService } from '../profile.service';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
-  styleUrls: ['./search.component.css']
+  styleUrls: ['./search.component.css'],
 })
 export class SearchComponent implements OnInit {
-  profile: Profile = new Profile;
+  profile: Profile = new Profile();
 
-  constructor() { }
+  constructor(private profileService: ProfileService) {}
 
-  ngOnInit(): void {
-    
+  ngOnInit(): void {}
+  onSubmit(userName:string){
+console.log(userName)
+this.profileService.getUser(userName).then((result)=>{
+  console.log(result)
+});
   }
 
+  
 }
