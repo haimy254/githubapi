6dbbdeb8c80f6456f0037f21cca542428c059023
haimy254 <haimanaare@gmail.com>
2022-04-22 08:55:00 +0300

    repo serv

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,5 +1,6 @@
 
 import { Component, OnInit } from '@angular/core';
+import { Repo } from '../repo';
 
 
 @Component({
--- src/app/repo.service.ts
+++ src/app/repo.service.ts
@@ -1,11 +1,19 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Repo } from './repo';
+import { lastValueFrom } from 'rxjs';
+
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class RepoService {
-  repos! : Repo [];
+  repos!: Repo[];
+  BaseUrl = 'https://api.github.com';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
+
+  async getUser(userName: string) {
+    let response = this.http.get(`${this.BaseUrl}/users/${userName}`);
+    return await lastValueFrom(response).then((result) => result);
+  }
 }
